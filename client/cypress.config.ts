a6039d19c2b48e4e761d5e110c17bf4c9e504ae1
1canas <pedro.s.furlaneto@gmail.com>
2024-11-22 11:45:44 -0300

    fix(npm workspace): Fix on root package.json, adding the workspace scripts for dev and test cases

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173", // Ajuste para o endereço correto do frontend
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:5173"
   },
 });
